d13c6dae5cf1fd3d21eaf1cc836eec9666886b8a
v_wupeng <v_wupeng@baidu.com>
2018-08-15 20:02:34 +0800

    Initial commit

---
+++ src/com/immortal/half/wu/Main.java
@@ -0,0 +1,63 @@
+package com.immortal.half.wu;
+
+
+import java.sql.*;
+
+
+public class Main {
+
+
+    static {
+        try {
+            String driverClass = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driverClass);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        SqlUtl sqlUtl = new SqlUtl();
+//        try {
+
+        String sqlUrl = "jdbc:mysql://localhost:3306/java_sql?useSSL=false&serverTimezone=GMT%2B8";
+        String sqlUser = "root";
+        String sqlPassWord = "mysql2b";
+
+        log("连接数据库");
+        try {
+            connection = DriverManager.getConnection(sqlUrl, sqlUser, sqlPassWord);
+
+            sqlUtl.queryAllUser(connection);
+            sqlUtl.addUserInfo(connection, 11, "newUserName", "newPassWord");
+            sqlUtl.queryAllUser(connection);
+            sqlUtl.updataUser(connection, 11, "changeUserName", "changePassWord");
+            sqlUtl.queryAllUser(connection);
+            sqlUtl.removeUserInfo(connection, 11, "changeUserName", "changePassWord");
+            sqlUtl.queryAllUser(connection);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println("Hello MySql!");
+
+    }
+
+
+    private static void log(String text) {
+        System.out.println(text);
+    }
+
+
+}
